b9517ef0cc8749f3188285cd478b051053200fbc
Nina <nina.emiko.mcnair@gmail.com>
2023-05-22 20:03:20 -0400

    Users must be signed in to have access to app functionality

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -21,18 +21,23 @@ export const Home = () => {
         {account?.login ? (
           <div>
             <Alert color="success">Welcome {account.login}.</Alert>
+            <button type="button" className="btn btn-success">
+              Schedule an Appointment
+            </button>
+            <div className="divider" />
+            <button type="button" className="btn btn-danger">
+              Add a Vehicle
+            </button>
+            <p className="lead">Make car appointments Fair!</p>
           </div>
         ) : (
           <div>
             <Alert color="warning">
-              If you want to
+              Welcome back! Please
               <span>&nbsp;</span>
               <Link to="/login" className="alert-link">
-                sign in
+                sign in.
               </Link>
-              , you can try the default accounts:
-              <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;) <br />- User (login=&quot;user&quot; and
-              password=&quot;user&quot;).
             </Alert>
 
             <Alert color="warning">
